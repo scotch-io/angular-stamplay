dcad56899c87debe4e9c5a27186225a57d884f89
Chris Sevilleja <chris@scotch.io>
2015-06-09 08:00:37 -0700

    adding comments

--- app/services/suggestion.js
+++ app/services/suggestion.js
@@ -29,9 +29,11 @@ function SuggestionService($q, $stamplay) {
     return deferred.promise;
   }
 
+  // get all the suggestions
   function getAll() {
     var deferred = $q.defer();
 
+    // use collection instead of model to grab all
     suggestionCollection.fetch()
       .then(function() {
         deferred.resolve(suggestionCollection);
@@ -40,12 +42,15 @@ function SuggestionService($q, $stamplay) {
     return deferred.promise;
   }
 
+  // vote on a suggestion (upvote or downvote)
   function vote(id, type) {
     var deferred = $q.defer();
 
+    // find the specific suggestion you want to vote on
     suggestionModel.fetch(id);
     console.log(suggestionModel);
 
+    // handle the upvote and the downvote
     if (type == 'upvote') {
       suggestionModel.upVote()
         .then(function() {
